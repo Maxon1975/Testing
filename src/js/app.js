ee0c325913592b262e13b809f557655f2d58c5f9
Annenkov Maksim <bakeeva@yandex.ru>
2023-10-21 13:29:52 +0300

    test

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import DOMControl from './componenst/DomControl'
+
+const widget = new DOMControl() //eslint-disable-line
---
+++ src/js/componenst/DomControl.js
@@ -0,0 +1,56 @@
+import Luhn from './algorithmLuhn/algorithmLuhn'
+import PaySistem from './validPaySistem/PaySistem'
+
+export default class DOMControl {
+  constructor () {
+    this.form = document.querySelector('.card__form')
+    this.input = document.querySelector('.card__number')
+
+    this.mir = document.querySelector('.mir')
+    this.visa = document.querySelector('.visa')
+    this.mastercard = document.querySelector('.master')
+
+    this.form.addEventListener('submit', this.onSubmit)
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    const value = this.input.value
+    this.input.classList.remove('input-not-valid')
+    this.input.classList.remove('input-valid')
+    document.querySelectorAll('.card__item').forEach(el => el.classList.add('not-valid'))
+    if (value.trim()) {
+      const checkLuhn = new Luhn(value)
+      const checkPaySistem = new PaySistem(value)
+      if (checkLuhn.valid) {
+        switch (checkPaySistem.paySistem) {
+          case 'mir':
+            this.mir.classList.remove('not-valid')
+            this.input.classList.add('input-valid')
+            return
+          case 'visa':
+            this.visa.classList.remove('not-valid')
+            this.input.classList.add('input-valid')
+            return
+          case 'mastercard':
+            this.mastercard.classList.remove('not-valid')
+            this.input.classList.add('input-valid')
+            return
+
+          default:
+            this.input.classList.add('input-not-valid')
+        }
+      } else {
+        this.input.classList.add('input-not-valid')
+      }
+    }
+  }
+}
+
+const widget = new DOMControl() // eslint-disable-line
+
+// const data = {
+//     'mir': '2202235433732147',
+//     'visa':'4278444840597105' ,
+//     'mastercard':'5360299162499025',
+//     }
